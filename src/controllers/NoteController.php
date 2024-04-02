64e7d6008f8f469d987d1084202e0511c7a7e695
zestx5 <buyankov.ilya@gmail.com>
2024-04-02 19:12:20 +0300

    add: notes endpoint

--- src/controllers/NoteController.php
+++ src/controllers/NoteController.php
@@ -2,7 +2,26 @@
 
 namespace Zeth\NotesPhp\Controllers;
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Zeth\NotesPhp\Repositories\InMemoryRepository;
+
 class NoteController
 {
+    private $repo;
 
+    public function __construct()
+    {
+        $this->repo = new InMemoryRepository();
+    }
+
+    public function notes(Request $request, Response $response, $args)
+    {
+        $data = $this->repo->getAll();
+        $payload = json_encode($data);
+
+        $response->getBody()->write($payload);
+        return $response
+            ->withHeader('Content-Type', 'application/json');
+    }
 }
--- src/index.php
+++ src/index.php
@@ -1,22 +1,12 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+
 use Slim\Factory\AppFactory;
+use Zeth\NotesPhp\Controllers\NoteController;
 use Zeth\NotesPhp\Models\Note;
-use Zeth\NotesPhp\Repositories\InMemoryRepository;
 
 $app = AppFactory::create();
-$repo = new InMemoryRepository();
-$repo->save(Note::createNote("Test"));
 
-$app->get('/note', function (Request $request, Response $response, $args) use ($repo) {
-    $data = $repo->getAll();
-    $payload = json_encode($data);
-
-    $response->getBody()->write($payload);
-    return $response
-        ->withHeader('Content-Type', 'application/json');
-});
+$app->get('/note', NoteController::class . ":notes");
 
 $app->run();
